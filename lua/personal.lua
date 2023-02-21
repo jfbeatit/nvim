e6e376f45dd09f5edd18119b74adb77362c2c4fb
jfbeatit <jfbeatit2023@gmail.com>
2023-02-21 19:30:06 +0800

    chore: update workspace dir

--- lua/personal.lua
+++ lua/personal.lua
@@ -1,7 +1,7 @@
 local api = vim.api
 
 api.nvim_create_user_command('Pd', function()
-  local cmd = "cd /workspace/www/mcenter/frontend && pnpm dev"
+  local cmd = "cd /workspace/www/xunhuawang/admin/frontend && pnpm dev"
   vim.fn.jobstart(cmd, {
     detach = false,
     pty = true,
