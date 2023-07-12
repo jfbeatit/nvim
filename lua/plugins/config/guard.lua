41e0391a3c49be2973bf74de33a51fb511f62ad0
jfbeatit <jfbeatit2023@gmail.com>
2023-07-12 15:45:10 +0800

    chore: update

--- lua/plugins/config/guard.lua
+++ lua/plugins/config/guard.lua
@@ -3,10 +3,11 @@ local ft = require('guard.filetype')
 ft('rust'):fmt('rustfmt')
 ft('javascript'):fmt('prettier')
 ft('typescript'):fmt('prettier')
+ft('typescriptreact'):fmt('prettier')
 ft('vue'):fmt('prettier')
 ft('html'):fmt('prettier')
 ft('css'):fmt('prettier')
-ft('json'):fmt('prettier')
+ft('jsonc'):fmt('prettier')
 
 require('guard').setup({
   fmt_on_save = true,
